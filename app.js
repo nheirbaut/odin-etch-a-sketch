8ec3a872d7dd059edbc709e18d4178e6ffa0ed46
Niels Heirbaut <niels@opilo.nl>
2024-12-11 09:07:58 +0100

    Create 16x16 grid

---
+++ app.js
@@ -0,0 +1,19 @@
+
+function createGrid(squaresPerRow) {
+    const gridContainer = document.getElementById('grid-container');
+    const gridContainerWidth = gridContainer.offsetWidth;
+    const squareSize = gridContainerWidth / squaresPerRow;
+
+    for (let i = 0; i < squaresPerRow * squaresPerRow; i++) {
+        const square = document.createElement('div');
+
+        square.classList.add('square');
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        gridContainer.appendChild(square);
+    }
+}
+
+createGrid(16);
