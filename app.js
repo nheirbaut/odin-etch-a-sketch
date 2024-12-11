1466ff5a2584a75deaff03f941c6d446358a5bfc
Niels Heirbaut <niels@opilo.nl>
2024-12-11 09:58:08 +0100

    Enable asking user for grid size

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+function setGridSize() {
+    const input = prompt("How many squares per edge? [1 - 100].");
+    const gridSize = parseInt(input, 10);
+
+    if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+        alert("Invalid input. Please enter an integer between 1 and 100.");
+        return;
+    }
+
+    createGrid(gridSize);
+}
+
 
 function createGrid(squaresPerRow) {
     const gridContainer = document.getElementById('grid-container');
+
+    gridContainer.innerHTML = '';
+
     const gridContainerWidth = gridContainer.offsetWidth;
     const squareSize = gridContainerWidth / squaresPerRow;
 
